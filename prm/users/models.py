31e2f55d3b8d1712ba793caa341449d638134e2a
Dmitry G <sdprecovery@gmail.com>
2023-03-04 16:02:26 +0300

    Migrate user: - Alter field avatar on User; - Alter field city on User; - Alter field date_of_birth on User; - Alter field metamask_wallet on User;
    Update profile form styles;

--- prm/users/models.py
+++ prm/users/models.py
@@ -13,10 +13,12 @@ class User(AbstractUser):
         blank=True,
         validators=[validate_phone_number],
     )
-    date_of_birth = models.DateField(blank=True, null=True)
-    city = models.CharField(max_length=50, blank=True)
-    avatar = models.ImageField(upload_to="avatars/", default="avatars/default.png")
-    metamask_wallet = models.CharField(max_length=155, blank=True)
+    date_of_birth = models.DateField(_("Дата рождения"), blank=True, null=True)
+    city = models.CharField(_("Город"), max_length=50, blank=True)
+    avatar = models.ImageField(
+        _("Аватар"), upload_to="avatars/", default="avatars/default.png"
+    )
+    metamask_wallet = models.CharField(_("Metamask"), max_length=155, blank=True)
 
     def get_absolute_url(self):
         return reverse("dashboard:index", kwargs={"username": self.username})
