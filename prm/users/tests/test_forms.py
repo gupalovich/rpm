27ee360df0a25e86196d8a220baf9554fa0f40bd
Dmitry G <sdprecovery@gmail.com>
2023-03-10 12:36:11 +0300

    Tests for UserSignupForm;

--- prm/users/tests/test_forms.py
+++ prm/users/tests/test_forms.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from ..forms import UserAdminCreationForm, UserSignupForm
+from .factories import UserFactory
 
 User = get_user_model()
 
@@ -26,6 +27,7 @@ class UserSignupFormTests(TestCase):
             "password1": "password",
             "password2": "test",
         }
+        self.url = reverse("account_signup")
 
     def test_form_valid(self):
         form = UserSignupForm(data=self.form_data)
@@ -33,6 +35,7 @@ class UserSignupFormTests(TestCase):
 
     def test_form_invalid(self):
         form = UserSignupForm(data=self.form_data_invalid)
+        # test errors
         self.assertEqual(len(form.errors), 5)
         self.assertEqual(form.errors["first_name"], ["This field is required."])
         self.assertEqual(form.errors["last_name"], ["This field is required."])
@@ -43,10 +46,21 @@ class UserSignupFormTests(TestCase):
             ["Номер телефона должен быть в формате: '8 (999) 999-99-99'."],
         )
 
+    def test_get_referral_field_populated(self):
+        """Test url /accounts/signup/?referral=admin populates referral value"""
+        referral_value = "admin"
+        response = self.client.get(self.url + f"?referral={referral_value}")
+        form = response.context["form"]
+        self.assertIsInstance(form, UserSignupForm)
+        self.assertEqual(form.initial["referral"], referral_value)
+
     def test_post_valid(self):
-        response = self.client.post(reverse("account_signup"), data=self.form_data)
+        response = self.client.post(self.url, data=self.form_data)
         self.assertRedirects(response, reverse("account_email_verification_sent"))
-        user = User.objects.first()
+        # get created user
+        user = User.objects.get(username=self.form_data["username"])
+        # test created user data
+        self.assertEqual(user.parent, None)
         self.assertEqual(user.first_name, self.form_data["first_name"])
         self.assertEqual(user.last_name, self.form_data["last_name"])
         self.assertEqual(user.phone_number, self.form_data["phone_number"])
@@ -54,9 +68,28 @@ class UserSignupFormTests(TestCase):
         self.assertEqual(user.username, self.form_data["username"])
 
     def test_post_invalid(self):
-        response = self.client.post(reverse("account_signup"), data={})
+        response = self.client.post(self.url, data={})
         self.assertTemplateUsed(response, "account/signup.html")
         self.assertFalse(User.objects.all())
+
+    def test_post_with_referral_valid(self):
+        parent = UserFactory()
+        self.form_data.update({"referral": parent.username})
+        # make POST request
+        response = self.client.post(self.url, data=self.form_data)
+        user = User.objects.get(username=self.form_data["username"])
+        # test created user data
+        self.assertRedirects(response, reverse("account_email_verification_sent"))
+        self.assertEqual(user.parent, parent)
+
+    def test_post_with_referral_invalid(self):
+        self.form_data.update({"referral": "123"})
+        # make POST request
+        response = self.client.post(self.url, data=self.form_data)
+        user = User.objects.get(username=self.form_data["username"])
+        # test created user data
+        self.assertRedirects(response, reverse("account_email_verification_sent"))
+        self.assertEqual(user.parent, None)
 
 
 class TestUserAdminCreationForm:
